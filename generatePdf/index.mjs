6f27a841872b89b8e7ac1b8e86ae2edfbafebed7
Seokmin Hong <ghdtjrald240@gmail.com>
2022-06-16 18:08:57 +0900

    Add logs

--- generatePdf/index.mjs
+++ generatePdf/index.mjs
@@ -8,22 +8,32 @@ import handler from "serve-handler";
 
 const PORT = 8125;
 
-const server = (() =>
-  createServer((req, res) =>
+const server = (() => {
+  console.log("Start Server");
+  return createServer((req, res) =>
     handler(req, res, {
       cleanUrls: true,
       public: "..",
     })
-  ).listen(PORT))();
+  ).listen(PORT);
+})();
 
 const files = await promisify(glob)("../presentations/*.html");
 
+console.log("Launch Puppeteer");
 const browser = await puppeteer.launch();
 const printPdfs = files.map(async (file) => {
+  console.log(`Create page for ${file}`);
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
 
   const filePath = path.resolve(process.cwd(), file);
+  console.log(
+    `Route to http://localhost:${PORT}/presentations/${path.basename(
+      filePath,
+      ".html"
+    )}?print-pdf`
+  );
   await page.goto(
     `http://localhost:${PORT}/presentations/${path.basename(
       filePath,
@@ -33,9 +43,11 @@ const printPdfs = files.map(async (file) => {
       waitUntil: "networkidle2",
     }
   );
+  console.log("Wait for revealLoaded");
   await page.waitForFunction("window.revealLoaded");
 
   mkdirSync("../pdfs", { recursive: true });
+  console.log("Generate PDF");
   await page.pdf({
     format: "a4",
     path: `../pdfs/${path.basename(filePath, ".html")}.pdf`,
